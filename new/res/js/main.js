a70a01b0b30485d5c9b299da0f0917071625f862
MahbubTonoy <tnymrbadboy@gmail.com>
2021-12-31 23:56:16 +0600

    some libraries added

---
+++ new/res/js/main.js
@@ -0,0 +1,24 @@
+//full page
+new fullpage('#fullpage', {
+ //navigation
+ anchors: ['MahbubTonoy', 'about', 'skills', 'services', 'background', 'projects', 'contact'],
+ navigation: true,
+ navigationTooltips: ['MahbubTonoy', 'About', 'Skills', 'Services', 'Background', 'Projects', 'Contact', 'Footer'],
+
+ //scroll
+ scrollingSpeed: 500,
+
+ //design
+ verticalCentered: false,
+
+});
+
+
+//particleground
+particleground(document.getElementById('homePage'), {
+ dotColor: '#fff',
+ lineColor: '#fff',
+});
+
+
+//typed text
